69f9ef50e456ec1e11b90986af903fcee44f4ddb
Harrison Jones <harrisonjones@Harrisons-iMac.local>
2019-06-26 21:59:45 -0400

    added visual que for search filter

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,6 +13,7 @@ class SearchBar extends React.Component {
     }
     this.handleSortByChange = this.handleSortByChange.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.sortByOptions = {
       'Best Match':'best_match',
       'Highest Rated':'highest_rated',
@@ -46,6 +47,11 @@ class SearchBar extends React.Component {
     });
   }
 
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
+  }
+
   renderSortByOptions() {
     return(
     Object.keys(this.sortByOptions).map(sortByOption => {
@@ -65,11 +71,17 @@ class SearchBar extends React.Component {
     </ul>
   </div>
   <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
+    <input
+      placeholder="Search Businesses"
+      onChange={this.handleTermChange}
+     />
+    <input
+      placeholder="Where?"
+      onChange={this.handleLocationChange}
+     />
   </div>
   <div className="SearchBar-submit">
-    <a>Let's Go</a>
+    <a onClick={this.handleSearch}>Let's Go</a>
   </div>
 </div>
 
